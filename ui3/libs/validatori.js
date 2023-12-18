e7a51a1ac38c3f6c93a8e100452e0b4e82a2ec4f
madbad <madbad82@gmail.com>
2023-12-18 22:21:49 +0100

    webcomponents test

---
+++ ui3/libs/validatori.js
@@ -0,0 +1,17 @@
+<tipidicampo>
+NUMERODOC = {
+	tipo'=>'INTEGER'
+	'lunghezza'=>6;
+}
+DATA = 		tipo'=>'INTEGER', 'lunghezza'=>10));
+CODICE = 	tipo'=>'TEXT',   'lunghezza'=>6));
+IMPORTO = 	tipo'=>'REAL',   'lunghezza'=>10));
+TESTO = 	tipo'=>'TEXT',   'lunghezza'=>''));
+NUMERO = 	tipo'=>'INTEGER','lunghezza'=>6));
+ALBO = 		tipo'=>'TEXT', '  lunghezza'=>20));
+PARTITAIVA = tipo'=>'INTEGER','lunghezza'=>11));
+CODFISCALE = tipo'=>'TEXT',   'lunghezza'=>16));
+TELEFONO = 	tipo'=>'TEXT',   'lunghezza'=>11));
+MAIL = 		tipo'=>'TEXT',   'lunghezza'=>30));
+URL = 		tipo'=>'TEXT',   'lunghezza'=>50));
+</tipidicampo>
